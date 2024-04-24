166566cf4ac8aa249d7856b292089f2020ef19a9
joeywwwu <joeywwwu@outlook.com>
2024-04-23 22:34:34 -0700

    1

--- src/app.py
+++ src/app.py
@@ -1,1 +1,2 @@
 # No need for app.py because it is a Tableau dashboard
+# Go to the following URL: https://public.tableau.com/app/profile/wenwei.wu6394/viz/532IndividualAssignment-Dashboard/Dashboard1?publish=yes.
